bfc63dbd0a247389a8492db1e5ef1b4eef5945bc
Lumir Balhar <lbalhar@redhat.com>
2021-11-08 12:33:47 +0100

    fixup! 00251: Change user install location

--- Lib/sysconfig.py
+++ Lib/sysconfig.py
@@ -61,6 +61,10 @@ _INSTALL_SCHEMES = {
 # backup the original posix_prefix as rpm_prefix
 # RPM packages use it and we need to be able to read it even when changed
 _INSTALL_SCHEMES['rpm_prefix'] = _INSTALL_SCHEMES['posix_prefix']
+# Virtualenv from 20.10.0 favors a new "venv" scheme over the defaults.
+# See: https://github.com/pypa/virtualenv/commit/8da79db86d8a5c74d03667a40e64ff832076445e
+# "venv" should be the same as the default for us.
+_INSTALL_SCHEMES['venv'] = _INSTALL_SCHEMES['posix_prefix']
 
 if (not (hasattr(sys, 'real_prefix') or
     sys.prefix != sys.base_prefix) and
