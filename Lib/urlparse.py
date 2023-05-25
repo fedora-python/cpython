582b28b32c9921b30fed62a7de1b70e5526b69f5
Lumir Balhar <lbalhar@redhat.com>
2023-05-25 10:03:57 +0200

    00399-cve-2023-24329.patch
    00399 #
    
    * gh-102153: Start stripping C0 control and space chars in `urlsplit` (GH-102508)
    
    `urllib.parse.urlsplit` has already been respecting the WHATWG spec a bit GH-25595.
    
    This adds more sanitizing to respect the "Remove any leading C0 control or space from input" [rule](https://url.spec.whatwg.org/GH-url-parsing:~:text=Remove%20any%20leading%20and%20trailing%20C0%20control%20or%20space%20from%20input.) in response to [CVE-2023-24329](https://nvd.nist.gov/vuln/detail/CVE-2023-24329).
    
    Backported to Python 2 from Python 3.12.
    
    Co-authored-by: Illia Volochii <illia.volochii@gmail.com>
    Co-authored-by: Gregory P. Smith [Google] <greg@krypto.org>
    Co-authored-by: Lumir Balhar <lbalhar@redhat.com>

--- Lib/urlparse.py
+++ Lib/urlparse.py
@@ -26,6 +26,10 @@ scenarios for parsing, and for backward compatibility purposes, some
 parsing quirks from older RFCs are retained. The testcases in
 test_urlparse.py provides a good indicator of parsing behavior.
 
+The WHATWG URL Parser spec should also be considered.  We are not compliant with
+it either due to existing user code API behavior expectations (Hyrum's Law).
+It serves as a useful guide when making changes.
+
 """
 
 import re
@@ -62,6 +66,10 @@ scheme_chars = ('abcdefghijklmnopqrstuvwxyz'
                 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
                 '0123456789'
                 '+-.')
+
+# Leading and trailing C0 control and space to be stripped per WHATWG spec.
+# == "".join([chr(i) for i in range(0, 0x20 + 1)])
+_WHATWG_C0_CONTROL_OR_SPACE = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f '
 
 # Unsafe bytes to be removed per WHATWG spec
 _UNSAFE_URL_BYTES_TO_REMOVE = ['\t', '\r', '\n']
@@ -201,6 +209,8 @@ def urlsplit(url, scheme='', allow_fragments=True):
     (e.g. netloc is a single string) and we don't expand % escapes."""
     url = _remove_unsafe_bytes_from_url(url)
     scheme = _remove_unsafe_bytes_from_url(scheme)
+    url = url.lstrip(_WHATWG_C0_CONTROL_OR_SPACE)
+    scheme = scheme.strip(_WHATWG_C0_CONTROL_OR_SPACE)
     allow_fragments = bool(allow_fragments)
     key = url, scheme, allow_fragments, type(url), type(scheme)
     cached = _parse_cache.get(key, None)
