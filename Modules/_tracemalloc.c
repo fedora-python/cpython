b55c2d97ff133e474f624adf9a5c39637fb3fce0
Petr Viktorin <encukou@gmail.com>
2026-02-09 10:44:21 +0100

    00477: Raise an error when importing stdlib modules compiled for a different Python version
    This is a downstream workaround "implementing" python#137212 -
    the mechanism for the check exists in Python 3.15+, where it needs to be
    added to the standard library modules.
    In Fedora, we need it also in previous Python versions, as we experience
    segmentation fault when importing stdlib modules after update while
    Python is running.
    
    _curses, _tkinter, _tracemalloc and readline are not calling PyModuleDef_Init,
    which is modified with this patch, hence they need a
    direct call to the check function.
    
    Co-Authored-By: Karolina Surma <ksurma@redhat.com>

--- Modules/_tracemalloc.c
+++ Modules/_tracemalloc.c
@@ -215,6 +215,12 @@ static struct PyModuleDef module_def = {
 PyMODINIT_FUNC
 PyInit__tracemalloc(void)
 {
+    #ifdef _PyHack_check_version_on_modinit
+        if (_PyHack_CheckInternalAPIVersion("_tracemalloc") < 0) {
+            return NULL;
+        }
+    #endif
+
     PyObject *m;
     m = PyModule_Create(&module_def);
     if (m == NULL)
