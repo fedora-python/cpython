dd54be382f95ecf4329d64ed7b7e79950bd488fd
Miro Hrončok <miro@hroncok.cz>
2026-04-16 14:39:41 +0200

    00486: gh-148646: Add --enable-prebuilt-jit-stencils configure flag

--- Tools/jit/_targets.py
+++ Tools/jit/_targets.py
@@ -205,6 +205,7 @@ class _Target(typing.Generic[_S, _R]):
         *,
         comment: str = "",
         force: bool = False,
+        prebuilt: bool = False,
         jit_stencils: pathlib.Path,
     ) -> None:
         """Build jit_stencils.h in the given directory."""
@@ -218,7 +219,7 @@ class _Target(typing.Generic[_S, _R]):
         if (
             not force
             and jit_stencils.exists()
-            and jit_stencils.read_text().startswith(digest)
+            and (prebuilt or jit_stencils.read_text().startswith(digest))
         ):
             return
         stencil_groups = ASYNCIO_RUNNER.run(self._build_stencils())
